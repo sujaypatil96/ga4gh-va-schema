c23e4153ea5e3b3fcf3356ce50db94ab91805d6d
Sujay Patil <sujaysanjeev.patil@gmail.com>
2023-04-18 09:42:22 -0700

    chore: initial commit

--- src/ga4gh_va_schema/datamodel/ga4gh_va_schema.py
+++ src/ga4gh_va_schema/datamodel/ga4gh_va_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from ga4gh_va_schema.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-04-18T09:41:30
+# Generation date: 2023-04-18T09:42:11
 # Schema: GA4GH-VA-Core-IM
 #
 # id: https://w3id.org/ga4gh-va-core-im
